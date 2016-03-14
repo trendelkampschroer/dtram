6e4ddfb405013c66d269fb64ca0a6ccc379166d3
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2016-03-14 17:24:21 +0100

    Add license info

--- dtram/linsolve_old.py
+++ dtram/linsolve_old.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of dtram.
+
+# Copyright (c) 2016 Benjamin Trendelkamp-Schroer
+
+# dtram is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# dtram is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with dtram.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 from scipy.linalg import lu_factor, lu_solve
 from scipy.sparse import issparse, diags, csr_matrix
