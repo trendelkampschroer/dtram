b865ca050773e46b7e30eef04ae1bc15cf126bf3
Benjamin Trendelkamp-Schroer <benjamin.trendelkamp-schroer@fu-berlin.de>
2016-03-14 16:21:51 +0100

    Add extension

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+
+ext_objective_sparse = Extension("dtram.objective_sparse",
+                                 sources=["dtram/objective_sparse.pyx",],
+                                 libraries=["m",])
 
 setup(name="dtram",
       version = "0.0.1",
@@ -7,6 +11,7 @@ setup(name="dtram",
       author = "Benjamin Trendelkamp-Schroer",
       author_email = "Benjamin.Trendelkamp-Schroer@fu-berlin.de",
       packages = find_packages(),
+      ext_modules=[ext_objective_sparse,],
       install_requires = ['numpy>=1.7.1', 
                           'scipy>=0.11']
       )
